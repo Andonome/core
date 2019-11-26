0cf339db1171301263823b36aa4b9947aac6cacd
Malin Freeborn <malinfreeborn@tutamail.com>
2019-11-26 05:56:57 +0100

    make autocompile check files first

--- autocompile.sh
+++ autocompile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-git subtree -P config pull ../config master
-git subtree -P config push ../config fb
-pdflatex First_Blood.tex
-pdflatex First_Blood.tex
+[ -d ../config ] && git subtree -P config pull ../config master && git subtree -P config push ../config fb
+# pdflatex First_Blood.tex
+# pdflatex First_Blood.tex
 makeindex First_Blood.idx
 makeglossaries First_Blood
+# pdflatex First_Blood.tex
