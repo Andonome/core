217973e8fef6487f011666ece363accfab4e04dd
Malin Freeborn <malinfreeborn@tutamail.com>
2020-04-30 16:33:14 +0200

    make autocompile symlink to config/autocompile.sh

--- autocompile.sh
+++ autocompile.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-if [ $1 = clean ]; then
-	rm *aux *log main.{i,g}* *toc
-	exit 0
-fi
-
-if [ $1 = tree ]; then
-	git subtree -P config pull ../config master && \
-	git subtree -P config push ../config core
-fi
-pdflatex main.tex
-pdflatex main.tex
-makeindex main.idx
-makeglossaries main
-pdflatex main.tex
+config/autocompile.sh
